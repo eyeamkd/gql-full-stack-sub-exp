a0795f6e0e595ed0913c5a87a153d68d80441d5e
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-21 18:05:12 +0530

    React Testing library scripts WIP

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -2,21 +2,61 @@ import React from "react";
 import { Grid, Box, TextField, Button, Paper, Typography } from "@mui/material";
 
 function Post() {
+  const [title, setTitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
+
+  const submitPost = () => {
+    console.log("Title is", title, "Description is ", description);
+  };
+
   return (
-    <Box sx={{ display:'flex', alignItems:'center', justifyContent:'center', alignContent:'center'}}> 
-      <Paper sx={{padding:'20px',maxWidth:'md'}} variant="elevation" elevation={3}> 
-        <Grid container spacing={2} direction="column" sx={{justifyContent:'center', alignItems:'center'}}> 
-          <Grid item>  
-            <Typography variant="h4" align="center" >New Post</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        alignContent: "center",
+      }}
+    >
+      <Paper
+        sx={{ padding: "20px", maxWidth: "md" }}
+        variant="elevation"
+        elevation={3}
+      >
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          sx={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <Grid item>
+            <Typography variant="h4" align="center">
+              New Post
+            </Typography>
           </Grid>
           <Grid item>
-            <TextField variant="outlined" placeholder="Title" />
+            <TextField
+              variant="outlined"
+              placeholder="Title"
+              onChange={(e) => setTitle(e.target.value)} 
+              inputProps={{"data-testid":"post-input-title"}}
+            />
           </Grid>
           <Grid item>
-            <TextField  rows={5} size="normal" variant="outlined" multiline placeholder="Description" />
+            <TextField
+              rows={5}
+              size="normal"
+              variant="outlined"
+              multiline
+              placeholder="Description"
+              onChange={(e) => setDescription(e.target.value)}
+              inputProps={{"data-testid":"post-input-description"}}
+            />
           </Grid>
           <Grid item>
-            <Button variant="contained">Submit</Button>
+            <Button variant="contained" onClick={submitPost}>
+              Submit
+            </Button>
           </Grid>
         </Grid>
       </Paper>
--- client/src/components/Post/post.test.js
+++ client/src/components/Post/post.test.js
@@ -1,12 +1,25 @@
-import React from 'react'; 
-import {render, screen} from '@testing-library/react'; 
-import Post from './index';  
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react"; 
+import userEvent from '@testing-library/user-event';
+import Post from "./index";
 
-describe('Post Component', () => {
-    test('Basic Rendering of Post component',()=>{
-        render(<Post/>); 
-        screen.debug();
-    })
-})
+describe("Post Component", () => {
+  test("Basic Rendering of Post component", () => {
+    render(<Post />);
+    expect(screen.getByPlaceholderText("Title")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Description")).toBeInTheDocument();
 
+    fireEvent.change(screen.getByTestId("post-input-title"), {
+      target: { value: "Post Title" },
+    });
 
+    fireEvent.change(screen.getByTestId("post-input-description"), {
+      target: { value: "Post Description" },
+    }); 
+
+    userEvent.type(screen.getByTestId("post-input-title"),'Post Title'); 
+    userEvent.type(screen.getByTestId("post-input-description"),'Post Description');
+    screen.debug();
+    expect(screen.getByText("Submit")).toBeInTheDocument();
+  });
+});
