54c1cab12e6d429a4d55d272707c402b72d544d4
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-21 12:47:50 +0530

    added client

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send("Server running").status(200); 
-}) 
+})  
+
+//dummy comment to check git
 
 app.listen(4000,()=>{
     console.log("Server listening at 4000")
